bdf25ee55903d8a047d759d0ccdf085c3dcc0ccf
Crysta <crysta.frost@gmail.com>
2017-04-14 10:38:10 -0700

    Starting to define some variables for my timer tool.

--- WorkTimer/WorkTimer.py
+++ WorkTimer/WorkTimer.py
@@ -7,3 +7,15 @@
 
 #cmf: Trying to add an edit to the repository from VS
 
+import tkinter
+import datetime
+
+m_Today = datetime.datetime.today()
+
+m_MaxWorkTime = datetime.time(1, 0, 0, 0)
+m_MinWorkTime = datetime.time(0, 20, 0, 0)
+
+m_MaxBreakTime = datetime.time(0, 30, 0, 0)
+m_MinBreakTime = datetime.time(0, 5, 0, 0)
+
+print(str(m_Today.hour) + ":" + str(m_Today.minute))
