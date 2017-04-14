2538dfdf5ca0518814e66a90c9b2254c8e21831d
blumenkohlfrau <crysta.frost@gmail.com>
2017-04-14 09:50:47 -0700

    Added the initial python files for my timer
    Wrote the summary of the python product that I think I want by the end by adding comments to the top of the python file.

---
+++ WorkTimer/WorkTimer.py
@@ -0,0 +1,7 @@
+#cmf: Summary
+#   -timer that has a set "work" time and "break" time. 
+#   -Default work time = 30 min (no greater than an hour), break time = no less than 5 min.
+#   -Window popup for the duration of the break. 
+#   -Makes a system sound? 
+#   -Window that allows you to choose times for work and break.
+
